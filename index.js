eabb9d9badaeb439b8a8e7b9fdd5feaf8106a60c
brunabbomfim <brunabomfim.bbb@gmail.com>
2022-05-02 22:28:46 -0300

    procfile

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express'); //Declaração require para importar o express
 require('dotenv').config();//Declaração require para importar o dotenv
+const express = require('express'); //Declaração require para importar o express
 const cors = require('cors'); //Declaração require para importar o cors
-const port = process.env.PORT || 3000;
 const app = express(); // Execução do express
 const route = require('./src/routes/personagens.routes'); //Declaração require para importar a pasta routes
 const connectToDatabase = require('./src/database/database'); //Declaração require para importar a pasta database
+const port = process.env.PORT || 3000;
 connectToDatabase(); // Chamando a função
 app.use(cors()); // Reconhecendo o cors nas requisições
 app.use(express.json()); // Reconhecendo o formato JSON nas requisições
--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 function connectToDatabase() {
-    mongoose.connect(process.env.URI_DATABASE, {
-        useNewUrlParser: true,
+    mongoose
+    .connect(process.env.URI_DATABASE, {
+        useNewUrlParser: true, 
         useUnifiedTopology: true,
     })
     .then(() => {
