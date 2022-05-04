247f6cf333a6c827d1d6af9de681fc21294b288f
brunabbomfim <brunabomfim.bbb@gmail.com>
2022-05-03 21:49:12 -0300

    comit

--- src/routes/personagens.routes.js
+++ src/routes/personagens.routes.js
@@ -9,12 +9,12 @@ const personagensController = require('../controllers/personagens.controller');
 const { validId , validObjectBody } = require('../middlewares/personagens.middlewares')
 
 // Chamando as funções do controller
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 router.get('/', personagensController.findPersonagensController);
 router.get('/find/:id', validId, personagensController.findPersonagemByIdController);
 router.post('/create', validObjectBody, personagensController.createPersonagemController);
 router.put('/update/:id', validId , validObjectBody, personagensController.updatePersonagemController);
-router.delete('/delete/:id', validId, personagensController.deletePersonagemController)
+router.delete('/delete/:id', validId, personagensController.deletePersonagemController);
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 // Exportando as funções
 module.exports = router;
